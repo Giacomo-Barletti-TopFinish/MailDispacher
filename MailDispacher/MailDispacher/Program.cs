2e6b5014e46d42d7547b48b98d54cb022e9a299a
Giacomo Barletti <giacomo.barletti@kyklos-group.com>
2018-09-19 12:49:26 +0200

    Aggiunta solution e script folder

---
+++ MailDispacher/MailDispacher/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailDispacher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
